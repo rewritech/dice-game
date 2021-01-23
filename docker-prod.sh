b37f6e7b8331249b088214a9de375a10b2ff91cc
btpink <40204416+btpink-seo@users.noreply.github.com>
2021-01-24 01:04:04 +0900

    Update docker-prod.sh

--- docker-prod.sh
+++ docker-prod.sh
@@ -17,3 +17,12 @@ then
     docker-compose -f docker-compose-production.yml --env-file ./api/.env build
     docker-compose -f docker-compose-production.yml --env-file ./api/.env up -d
 fi
+
+if [ $1 == "deploy" ]
+then
+    sudo systemctl restart docker
+    docker image prune -a -f
+    docker-compose -f docker-compose-production.yml --env-file ./api/.env down
+    docker-compose -f docker-compose-production.yml --env-file ./api/.env build
+    docker-compose -f docker-compose-production.yml --env-file ./api/.env up -d
+fi
