cfc855fee61d997d8d66698d68ee6aaf6a91f1c8
btpink-seo <btpink.seo@gmail.com>
2021-01-24 19:03:48 +0900

    게임결과 정렬, ui 수정

--- client/src/app/components/end-game-modal/end-game-modal.component.ts
+++ client/src/app/components/end-game-modal/end-game-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core'
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import { Room } from '../../types'
 import { I18nService } from '../../services/i18n.service'
 
@@ -13,6 +14,8 @@ export class EndGameModalComponent implements OnInit {
   @Input() room: Room
   @ViewChild('content') content: HTMLElement
 
+  heart = faHeart
+
   constructor(
     private config: NgbModalConfig,
     private modalService: NgbModal,
@@ -23,10 +26,20 @@ export class EndGameModalComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // killedPlayer 내림차순 소트
+    // killedPlayer 내림차순 && life 오름차순 && life가 0이면 꼴찌 소트
     this.room.players.sort((a, b): number => {
+      if (b.killedPlayer === a.killedPlayer) {
+        if (a.life > b.life) {
+          return -1
+        }
+        return a.life < b.life ? 1 : 0
+      }
       return b.killedPlayer - a.killedPlayer
     })
+    const defeat = this.room.players.findIndex((p) => p.life === 0)
+    if (defeat > -1) {
+      this.room.players.push(this.room.players.splice(defeat, 1)[0])
+    }
   }
 
   ngAfterViewInit(): void {
